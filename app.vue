9f59a33a920781b3162ebbabacb5f666e2ccc6fd
Hosana <hosanabarceloscandido@gmail.com>
2024-08-03 16:52:32 -0300

    feat: add primeira versão de app

--- app.vue
+++ app.vue
@@ -1,6 +1,42 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div id="app">
+    <TimerComponent />
   </div>
 </template>
+
+  <script>
+import TimerComponent from "./components/timer-component.vue";
+
+export default {
+  name: "App",
+  components: {
+    TimerComponent,
+  },
+};
+</script>
+
+<style>
+html {
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+
+body {
+  font-family: sans-serif;
+  padding: 0;
+  margin: 0;
+}
+
+#app {
+  height: 100vh;
+  text-align: center;
+  padding: 10px;
+  margin: 0 auto;
+  overflow: hidden;
+}
+</style>
